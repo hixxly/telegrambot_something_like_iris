90a33655afba4f581990345ae6ca9ba50efb3d67
hixxly <monste4ek@yandex.ru>
2025-01-17 20:29:57 +0300

    Описание изменений

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import requests
 import telebot
 from telebot import types
 from random import choice
+import logging
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
 
 # Замените 'YOUR_BOT_TOKEN' на токен вашего бота
-TOKEN = '7475882217:AAGkI1ghSsDiZjfAqsj1Pcodd0tVRZD-OjY'  # Замените на токен вашего бота
+TOKEN = 'YOUR_BOT_TOKEN'  # Замените на токен вашего бота
 bot = telebot.TeleBot(TOKEN)
 
 # Создание сессии для отправки сообщений
@@ -18,7 +22,7 @@ def send_message(chat_id, text):
         response = session.post(url, data=data)
         response.raise_for_status()  # Raise an error for bad responses
     except requests.exceptions.RequestException as e:
-        print(f"Ошибка при отправке сообщения: {e}")
+        logging.error(f"Ошибка при отправке сообщения: {e}")
 
 # Хэндлер для команды /start
 @bot.message_handler(commands=['start'])
@@ -36,9 +40,13 @@ def ban_user(message):
         if user_status in ['administrator', 'creator']:
             send_message(message.chat.id, "Невозможно забанить администратора.")
         else:
-            bot.ban_chat_member(chat_id, user_id)  # Баним пользователя
-            username = message.reply_to_message.from_user.username or "неизвестный пользователь"
-            send_message(message.chat.id, f"Пользователь @{username} забанен.")
+            try:
+                bot.ban_chat_member(chat_id, user_id)  # Баним пользователя
+                username = message.reply_to_message.from_user.username or "неизвестный пользователь"
+                send_message(message.chat.id, f"Пользователь @{username} забанен.")
+                logging.info(f"Пользователь @{username} был забанен.")
+            except Exception as e:
+                logging.error(f"Ошибка при бане пользователя: {e}")
     else:
         send_message(message.chat.id, "Пожалуйста, используйте команду в ответ на сообщение пользователя.")
 
@@ -68,10 +76,20 @@ def link_handler(message):
             bot.ban_chat_member(chat_id, user_id)  # Бан пользователя
             username = message.from_user.username or "неизвестный пользователь"  # Защита от отсутствия username
             bot.send_message(chat_id, f"Пользователь @{username} нарушил правила группы и был забанен.")
+            logging.info(f"Пользователь @{username} был забанен за отправку ссылки.")
         except Exception as e:
-            print(f"Ошибка при бане пользователя: {e}")
+            logging.error(f"Ошибка при бане пользователя: {e}")
     else:
         bot.send_message(chat_id, "Ссылки отправил администратор. Бан не применён.")
 
-# Запуск бота
-bot.infinity_polling(none_stop=True)
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    new_user = message.new_chat_member
+    chat_id = message.chat.id
+    user_id = new_user.id
+
+    bot.send_message(chat_id, 'Я принял нового пользователя!')
+    bot.approve_chat_join_request(chat_id, user_id)
+
+    # Получаем статус пользователя
+    user_status = bot.get_chat_member(chat_id, user_id).status
